3c0997a80c782052942d895054832a287e6537e4
amir-sediqi69 <amir.sediqi69@gmail.com>
2023-11-01 17:25:04 +0100

    UI crud functionality

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,28 +3,68 @@ import axios from 'axios'
 import { Container, Header, Icon, List } from 'semantic-ui-react'
 import { Activity } from '../../models/activity'
 import NavBar from './NavBar'
-
+import ActivityDashboard from '../../features/activites/dashboard/ActivityDashboard'
+import { v4 as uuid } from 'uuid';
 function App() {
 
 const[activities, setActivities] = useState<Activity[]>([])
+const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+const [editMode, setEditMode] = useState(false);
 useEffect(() => {
   axios.get<Activity[]>('http://localhost:5000/api/Activities').then(response => {
     setActivities(response.data)
   })
 }, [])
 
+function handleSelectActivity(id: string) {
+  setSelectedActivity(activities.find(x => x.id === id));
+}
+
+function handleCancelSelect() {
+  setSelectedActivity(undefined);
+}
+
+function handleFormOpen(id?: string) {
+  id ? handleSelectActivity(id) : handleCancelSelect();
+  setEditMode(true);
+}
+
+function handleFormClose() {
+  setEditMode(false);
+}
+
+function handleCreateOrEditActivity(activity: Activity) {
+  activity.id
+    ? setActivities([...activities.filter(x => x.id !== activity.id), activity])
+    : setActivities([...activities, { ...activity, id: uuid() }]);
+  setEditMode(false);
+  setSelectedActivity(activity);
+}
+
+function handleDeleteActivity(id: string) {
+  setActivities([...activities.filter(x => x.id !== id)])
+}
+
   return (
     <Fragment>  
-       <NavBar/>
+       <NavBar openForm={handleFormOpen}/>
        <Container style={{marginTop: '7em'}}>
-       <List>{activities.map(activity => (
-        <List.Item key={activity.id}>{activity.title}</List.Item>
-       ))}
-       </List>
+        <ActivityDashboard 
+         selectedActivity={selectedActivity}
+         selectActivity={handleSelectActivity}
+         cancelSelectActivity={handleCancelSelect}
+         activities={activities}
+         editMode={editMode}
+         openForm={handleFormOpen}
+         closeForm={handleFormClose}
+         createOrEdit={handleCreateOrEditActivity}
+         deleteActivity={handleDeleteActivity}
+        />
+        
        </Container>
 
     </Fragment>
   )
 }
 
-export default App
+export default App;
---
+++ client-app/src/features/activites/dashboard/ActivityList.tsx
@@ -0,0 +1,37 @@
+import { Segment, Item, Button, Label } from "semantic-ui-react";
+import { Activity } from "../../../models/activity";
+import React from "react";
+
+interface Props {
+    activities: Activity[];
+    selectActivity: (id: string) => void;
+    deleteActivity: (id: string) => void;
+}
+
+export default function ActivityList({activities, selectActivity, deleteActivity}: Props) {
+    return (
+        <Segment>
+            <Item.Group divided>
+                {activities.map(activity => (
+                    <Item key={activity.id}>
+                        <Item.Content>
+                            <Item.Header as='a'>{activity.title}</Item.Header>
+                            <Item.Meta>{activity.date}</Item.Meta>
+                            <Item.Description>
+                                <div>{activity.description}</div>
+                                <div>{activity.city}, {activity.venue}</div>
+                            </Item.Description>
+                            <Item.Extra>
+                            <Button floated='right' content='View' color='blue'
+                                        onClick={() => selectActivity(activity.id)}/>
+                               <Button floated='right' content='Delete' color='blue'
+                                        onClick={() => deleteActivity(activity.id)}/>
+                                <Label basic content={activity.category}/>
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>
+                ))}
+            </Item.Group>
+        </Segment>
+    )
+}
